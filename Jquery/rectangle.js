93891e71e2467e8a292c88ab49da5b9816d47988
Web Design <webdesign@Admins-iMac-2.local>
2019-05-22 20:53:46 +0300

    Curs Java 4

---
+++ Jquery/rectangle.js
@@ -0,0 +1,18 @@
+$('.rectangle').click(function() {
+	$(this).animate(
+		{
+		width:'0px',
+		height:'0px'
+		},
+		{
+			complete: function() {
+				$(this).animate(
+					{
+						width: '300px',
+						height: '100px'	
+					}
+				)
+			}
+		}
+	);
+});
---
+++ Jquery/speed.js
@@ -0,0 +1,43 @@
+
+$('#start').click(start);
+
+function start() {
+	$('#start').hide();
+	createNewLetter();
+}
+
+function createNewLetter() {
+	var letter = randomLetter();
+	var element = $('<div></div>')
+					.text(letter)
+					.css({
+						backgroundColor: randomColor(),
+						top: randomPosition(),
+						left: randomPosition(),
+
+					});
+	$('.container').append(element);
+}
+
+function randomLetter() {
+	var code = 97 + randomNumber(25);
+	var letter = String.fromCharCode(code);
+	return letter;
+}
+
+function randomPosition() {
+	return randomNumber(100) + '%';
+}
+
+function randomColor() {
+	var red = randomNumber(255);
+	var green = randomNumber(255);
+	var blue = randomNumber(255);
+	return 'rgb('+ red + ','+ green + ','+ blue + ')';
+}
+ 
+function randomNumber(max)  {
+	var number = Math.floor(Math.random() * (max + 1)); 
+	return number;
+}
+
